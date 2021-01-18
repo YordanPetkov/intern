e52e12865f489138f01e9fcd3f5a3bea170ae3bc
Yordan Petkov <yordan.petkov@grafixoft.com>
2021-01-18 18:14:17 +0200

    Task 8 from Defining Classes part 2 are half solved.

--- Tasks/Defining Classes - Part 2/08.Matrix/Matrix.cs
+++ Tasks/Defining Classes - Part 2/08.Matrix/Matrix.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace _08.Matrix
 {
-    class Matrix
+    class Matrix<T>
     {
+        T[,] matrix;
+
+        public Matrix(int n, int m)
+        {
+            matrix = new T[n, m];
+        }
+
+        public T this[int row, int col]
+        {
+            get { return matrix[row, col]; }
+            set { matrix[row, col] = value; }
+        }
+        public static Matrix<T> operator+(Matrix<T> matrix1, Matrix<T> matrix2)
+        {
+            try
+            {
+                if(matrix1.matrix.Length != matrix2.matrix.Length)
+                {
+                    throw new Exception("Operation addition cannot be performed.");
+                }
+            }
+        }
+
+
     }
 }
