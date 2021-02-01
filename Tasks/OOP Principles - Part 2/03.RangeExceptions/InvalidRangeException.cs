1877aa21045c288635ae8f00e514908f4835a901
Yordan Petkov <yordan.petkov@grafixoft.com>
2021-02-01 14:20:45 +0200

    All tasks from 'OOP principles part 2' are solved.

---
+++ Tasks/OOP Principles - Part 2/03.RangeExceptions/InvalidRangeException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.RangeExceptions
+{
+    class InvalidRangeException<T> where T : IComparable
+    {
+        private T start;
+        private T end;
+
+        public InvalidRangeException (T _start, T _end)
+        {
+            start = _start;
+            end = _end;
+        }
+
+        public bool CheckRange(T _value)
+        {
+            if(start.CompareTo(_value) <= 0 && _value.CompareTo(end) <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ Tasks/OOP Principles - Part 2/03.RangeExceptions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03.RangeExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InvalidRangeException<int> rangeOfNumbers = new InvalidRangeException<int>(1, 100);
+            DateTime startDate = new DateTime(1980, 1, 1);
+            DateTime endDate = new DateTime(2013, 12, 31);
+            InvalidRangeException<DateTime> rangeOfDates = new InvalidRangeException<DateTime>(startDate, endDate);
+            Console.WriteLine(rangeOfNumbers.CheckRange(12));
+            Console.WriteLine(rangeOfNumbers.CheckRange(190));
+            Console.WriteLine(rangeOfDates.CheckRange(new DateTime(2003, 03, 09)));
+            Console.WriteLine(rangeOfDates.CheckRange(new DateTime(2021, 02, 01)));
+        }
+    }
+}
