3df497e9b26c29f120af9fdaf103433bbed98d99
YordanPetkov <yordan_petkov180@abv.bg>
2021-02-03 17:00:44 +0200

    Make class 'Score' for fourth task from 'Naming Identifiers'.

---
+++ Tasks/Naming Identifiers/04.Re-factorAndImproveTheCode/Score.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.Re_factorAndImproveTheCode
+{
+	public class Score
+	{
+		string name;
+		int points;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public int Points
+		{
+			get { return points; }
+			set { points = value; }
+		}
+
+		public Score() { }
+
+		public Score(string name, int points)
+		{
+			this.name = name;
+			this.points = points;
+		}
+	}
+}
