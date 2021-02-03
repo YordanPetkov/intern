f6af5d5302b2f9509fd05e38b6e773ebf526d65a
Yordan Petkov <yordan.petkov@grafixoft.com>
2021-02-03 16:29:41 +0200

    First task from 'Formatting Code' is solved.

---
+++ Tasks/Naming Identifiers/01.class_123InCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.class_123InCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            inputValue();
+        }
+
+        public static void inputValue()
+        {
+            StringUtility stringUtility = new StringUtility();
+            stringUtility.parseBool(true);
+        }
+    }
+}
---
+++ Tasks/Naming Identifiers/01.class_123InCSharp/StringUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.class_123InCSharp
+{
+	class StringUtility
+	{
+		public void parseBool(bool value)
+		{
+			string parsedValue = value.ToString();
+			Console.WriteLine(parsedValue);
+		}
+	}
+}
