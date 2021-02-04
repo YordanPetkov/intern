cead6ddf54c86221d72eab586fbb4efea3308df9
YordanPetkov <yordan_petkov180@abv.bg>
2021-02-04 16:29:41 +0200

    Task from 'HQC Methods' is solved.

---
+++ Tasks/Methods/Methods/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string OtherInfo { get; set; }
+
+        public bool IsOlderThan(Student other)
+        {
+            if(other == null)
+            {
+                throw new Exception("Student instance is null.");
+            }
+            DateTime firstDate =
+                DateTime.Parse(this.OtherInfo.Substring(this.OtherInfo.Length - 10));
+            DateTime secondDate =
+                DateTime.Parse(other.OtherInfo.Substring(other.OtherInfo.Length - 10));
+            return firstDate > secondDate;
+        }
+    }
+}
