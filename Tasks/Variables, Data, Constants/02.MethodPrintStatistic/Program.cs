8bd41148118d9995a55368c5ddfd93bb08114d19
YordanPetkov <yordan_petkov180@abv.bg>
2021-02-04 14:39:59 +0200

    All tasks from 'Variables, Data, Constants' are solved.

---
+++ Tasks/Variables, Data, Constants/02.MethodPrintStatistic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02.MethodPrintStatistic
+{
+    class Program
+    {
+        public void PrintStatistics(double[] numbers, int countNumbers)
+        {
+            double maxNumber = double.MinValue;
+            double minNumber = double.MaxValue;
+            double sumNumbers = 0;
+            for (int i = 0; i < countNumbers; i++)
+            {
+                if (numbers[i] > maxNumber)
+                {
+                    maxNumber = numbers[i];
+                }
+
+                if (numbers[i] < minNumber)
+                {
+                    minNumber = numbers[i];
+                }
+
+                sumNumbers += numbers[i];
+            }
+
+            PrintMax(maxNumber);
+            PrintMin(minNumber);
+            PrintAvg(sumNumbers / countNumbers);
+        }
+
+    }
+}
