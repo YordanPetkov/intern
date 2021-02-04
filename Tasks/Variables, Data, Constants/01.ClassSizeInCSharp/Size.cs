259d2b1cfbb5d10f92a9ae19cf657fb43715d446
YordanPetkov <yordan_petkov180@abv.bg>
2021-02-04 14:32:04 +0200

    First task from 'Variables, Data, Constants is solved'.

---
+++ Tasks/Variables, Data, Constants/01.ClassSizeInCSharp/Size.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.ClassSizeInCSharp
+{
+    public class Size
+    {
+        public double width, height;
+        public Size(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public static Size GetRotatedSize(Size s, double angleOfRotation)
+        {
+            double widthAfterRotation = Math.Abs(Math.Cos(angleOfRotation)) * s.width + Math.Abs(Math.Sin(angleOfRotation)) * s.height;
+            double heightAfterRotation = Math.Abs(Math.Sin(angleOfRotation)) * s.width + Math.Abs(Math.Cos(angleOfRotation)) * s.height;
+            return new Size(widthAfterRotation, heightAfterRotation);
+        }
+    }
+}
