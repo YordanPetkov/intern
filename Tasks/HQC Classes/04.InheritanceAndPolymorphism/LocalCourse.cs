ebe72625b8be716eac52930234e1a624e4232dac
YordanPetkov <yordan_petkov180@abv.bg>
2021-02-04 17:37:27 +0200

    All tasks from 'HQC Classes' are solved.

---
+++ Tasks/HQC Classes/04.InheritanceAndPolymorphism/LocalCourse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.InheritanceAndPolymorphism
+{
+    public class LocalCourse : Course
+    {
+        public string Lab { get; set; }
+
+        public LocalCourse(string name) : base(name)
+        {
+            this.Lab = null;
+        }
+
+        public LocalCourse(string courseName, string teacherName) : base(courseName, teacherName)
+        {
+            this.Lab = null;
+        }
+
+        public LocalCourse(string courseName, string teacherName, IList<string> students) : base(courseName, teacherName, students)
+        {
+            this.Lab = null;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder(base.ToString());
+            if (this.Lab != null)
+            {
+                result.Append("; Lab = ");
+                result.Append(this.Lab);
+            }
+            result.Append(" }");
+            return result.ToString();
+        }
+    }
+}
---
+++ Tasks/HQC Classes/04.InheritanceAndPolymorphism/OffsiteCourse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.InheritanceAndPolymorphism
+{
+    public class OffsiteCourse : Course
+    {
+        
+        public string Town { get; set; }
+
+        public OffsiteCourse(string name) : base(name)
+        {
+            this.Town = null;
+        }
+
+        public OffsiteCourse(string courseName, string teacherName) : base(courseName, teacherName)
+        {
+            this.Town = null;
+        }
+
+        public OffsiteCourse(string courseName, string teacherName, IList<string> students) : base(courseName, teacherName, students)
+        {
+            this.Town = null;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder(base.ToString());
+            if (this.Town != null)
+            {
+                result.Append("; Town = ");
+                result.Append(this.Town);
+            }
+            result.Append(" }");
+            return result.ToString();
+        }
+    }
+}
