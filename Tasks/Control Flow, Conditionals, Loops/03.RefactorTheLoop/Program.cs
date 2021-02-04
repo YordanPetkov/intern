95ea14e48f5e5a3fbf34080bfc8437dfa85bf1b0
YordanPetkov <yordan_petkov180@abv.bg>
2021-02-04 16:04:52 +0200

    All tasks from 'Control Flow, Conditionals, Loops' are solved.

---
+++ Tasks/Control Flow, Conditionals, Loops/03.RefactorTheLoop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _03.RefactorTheLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int[] array = new int[100];
+            int expectedValue = 60;
+            for (i = 0; i < 100; i++)
+            {
+                array[i] = i;
+            }
+
+            for (i = 0; i < 100; i++)
+            {
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine(array[i]);
+                    if (array[i] == expectedValue)
+                    {
+                        i = 666;
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            // More code here
+            if (i == 666)
+            {
+                Console.WriteLine("Value Found");
+            }
+
+        }
+    }
+}
