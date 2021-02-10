6940af4b6db7b3dcce8a10f5285fe6ad200fbf5f
YordanPetkov <yordan_petkov180@abv.bg>
2021-02-10 14:37:31 +0200

    First task from 'Defensive Programming' is solved.

--- Tasks/Defensive Programming/01.Assertions/Properties/AssemblyInfo.cs
+++ Tasks/Defensive Programming/01.Assertions/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("460da7c6-019e-4d07-8b46-7a459405c58d")]
+[assembly: Guid("fc980d3a-6061-4fbb-ba37-a03dd4168750")]
 
 // Version information for an assembly consists of the following four values:
 //
