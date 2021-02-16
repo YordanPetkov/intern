716843bbc5b701aa4b95d4911bb8507f37a65a61
YordanPetkov <yordan_petkov180@abv.bg>
2021-02-16 14:34:52 +0200

    First task from 'Defensive programming' is solved.

--- Tasks/Defensive Programming/01.Assertions/AssertionsHomework.cs
+++ Tasks/Defensive Programming/01.Assertions/AssertionsHomework.cs
@@ -12,6 +12,7 @@ namespace _01.Assertions
         public static void SelectionSort<T>(T[] arr) where T : IComparable<T>
         {
             Debug.Assert(arr.Length > 0, "Array is empty!");
+
             for (int index = 0; index < arr.Length - 1; index++)
             {
                 int minElementIndex = FindMinElementIndex(arr, index, arr.Length - 1);
@@ -19,20 +20,22 @@ namespace _01.Assertions
             }
         }
 
-        private static int FindMinElementIndex<T>(T[] arr, int startIndex, int endIndex)
+        private static int FindMinElementIndex<T>(T[] array, int startIndex, int endIndex)
             where T : IComparable<T>
         {
-            Debug.Assert(arr.Length > 0, "Array is empty!");
-            Debug.Assert((startIndex >= 0 && startIndex < arr.Length), "Index was out of range. Must be non-negative and less than the size of the collection!");
-            Debug.Assert((endIndex >= 0 && endIndex < arr.Length), "Index was out of range. Must be non-negative and less than the size of the collection!");
+            Debug.Assert(array.Length > 0, "Array is empty!");
+            Debug.Assert((startIndex >= 0 && startIndex < array.Length), "Index was out of range. Must be non-negative and less than the size of the collection!");
+            Debug.Assert((endIndex >= 0 && endIndex < array.Length), "Index was out of range. Must be non-negative and less than the size of the collection!");
+            
             int minElementIndex = startIndex;
             for (int i = startIndex + 1; i <= endIndex; i++)
             {
-                if (arr[i].CompareTo(arr[minElementIndex]) < 0)
+                if (array[i].CompareTo(array[minElementIndex]) < 0)
                 {
                     minElementIndex = i;
                 }
             }
+
             return minElementIndex;
         }
 
@@ -43,57 +46,55 @@ namespace _01.Assertions
             y = oldX;
         }
 
-        public static int BinarySearch<T>(T[] arr, T value) where T : IComparable<T>
+        public static int BinarySearch<T>(T[] array, T value) where T : IComparable<T>
         {
-            Debug.Assert(arr.Length > 0, "Array is empty!");
-            return BinarySearch(arr, value, 0, arr.Length - 1);
+            Debug.Assert(array.Length > 0, "Array is empty!");
+
+            return BinarySearch(array, value, 0, array.Length - 1);
         }
 
-        private static int BinarySearch<T>(T[] arr, T value, int startIndex, int endIndex)
+        private static int BinarySearch<T>(T[] array, T value, int startIndex, int endIndex)
             where T : IComparable<T>
         {
-            Debug.Assert(arr.Length > 0, "Array is empty!");
-            Debug.Assert((startIndex >= 0 && startIndex < arr.Length), "Index was out of range. Must be non-negative and less than the size of the collection!");
-            Debug.Assert((endIndex >= 0 && endIndex < arr.Length), "Index was out of range. Must be non-negative and less than the size of the collection!");
+            Debug.Assert(array.Length > 0, "Array is empty!");
+            Debug.Assert((startIndex >= 0 && startIndex < array.Length), "Index was out of range. Must be non-negative and less than the size of the collection!");
+            Debug.Assert((endIndex >= 0 && endIndex < array.Length), "Index was out of range. Must be non-negative and less than the size of the collection!");
 
             while (startIndex <= endIndex)
             {
                 int midIndex = (startIndex + endIndex) / 2;
-                if (arr[midIndex].Equals(value))
+                if (array[midIndex].Equals(value))
                 {
                     return midIndex;
                 }
-                if (arr[midIndex].CompareTo(value) < 0)
+                if (array[midIndex].CompareTo(value) < 0)
                 {
-                    // Search on the right half
                     startIndex = midIndex + 1;
                 }
                 else
                 {
-                    // Search on the right half
                     endIndex = midIndex - 1;
                 }
             }
 
-            // Searched value not found
             return -1;
         }
 
         static void Main()
         {
-            int[] arr = new int[] { 3, -1, 15, 4, 17, 2, 33, 0 };
-            Console.WriteLine("arr = [{0}]", string.Join(", ", arr));
-            SelectionSort(arr);
-            Console.WriteLine("sorted = [{0}]", string.Join(", ", arr));
+            int[] numbers = new int[] { 3, -1, 15, 4, 17, 2, 33, 0 };
+            Console.WriteLine("arr = [{0}]", string.Join(", ", numbers));
+            SelectionSort(numbers);
+            Console.WriteLine("sorted = [{0}]", string.Join(", ", numbers));
 
-            SelectionSort(new int[0]); // Test sorting empty array
-            SelectionSort(new int[1]); // Test sorting single element array
+            SelectionSort(new int[0]);
+            SelectionSort(new int[1]);
 
-            Console.WriteLine(BinarySearch(arr, -1000));
-            Console.WriteLine(BinarySearch(arr, 0));
-            Console.WriteLine(BinarySearch(arr, 17));
-            Console.WriteLine(BinarySearch(arr, 10));
-            Console.WriteLine(BinarySearch(arr, 1000));
+            Console.WriteLine(BinarySearch(numbers, -1000));
+            Console.WriteLine(BinarySearch(numbers, 0));
+            Console.WriteLine(BinarySearch(numbers, 17));
+            Console.WriteLine(BinarySearch(numbers, 10));
+            Console.WriteLine(BinarySearch(numbers, 1000));
         }
     }
 }
