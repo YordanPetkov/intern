fb2c70199f9c5012f7d0a6dc54871ee87c957836
YordanPetkov <yordan_petkov180@abv.bg>
2021-03-15 13:57:15 +0200

    First 5 tasks from Entity Framework are solved.

---
+++ Tasks/Entity Framework/Station/Station.Models/Trip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Station.Models
+{
+    public class Trip
+    {
+        private ICollection<User> users;
+
+        public Trip()
+        {
+            this.users = new HashSet<User>();
+        }
+
+        public int Id { get; set; }
+        public string Place1 { get; set; }
+        public string Place2 { get; set; }
+
+        public int TrainId { get; set; }
+        public Train Train { get; set; }
+        public virtual ICollection<User> Users { get { return this.users; } set { this.users = value; } }
+    }
+
+}
---
+++ Tasks/Entity Framework/Station/Station.Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Station.Models
+{
+    public class User
+    {
+        private ICollection<Trip> trips;
+
+        public User()
+        {
+            this.trips = new HashSet<Trip>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Trip> Trips { get { return this.trips; } set { this.trips = value; } }
+    }
+}
